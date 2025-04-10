31d5be3239821c0bb685c32398b543d55c3f8f6d
Thomas Back <thomas.johan.back@gmail.com>
2025-04-09 18:05:32 -0700

    removing expo router

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,13 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack, useRouter, useSegments } from 'expo-router';
+import { useRouter, useSegments } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 import { PaperProvider } from 'react-native-paper';
 import { useColorScheme } from '@/hooks/useColorScheme';
-import { Avatar, Button, Card, Text } from 'react-native-paper';
 import { Session } from '@supabase/supabase-js';
 import { useAuth } from '@/lib/auth';
+import Home from './home';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -52,15 +50,7 @@ export default function RootLayout() {
 
   return (
     <PaperProvider>
-      <Card>
-        <Card.Title title="Welcome to Declutter" />
-        <Card.Content>
-          <Text variant="titleMedium">This will walk you through how to progress</Text>
-          <Button mode="contained" onPress={() => { }}>
-            Get Started
-          </Button>
-        </Card.Content>
-      </Card>
+      <Home />
     </PaperProvider >
   );
 }
---
+++ app/login.tsx
@@ -0,0 +1,8 @@
+import { useState } from 'react';
+import { supabase } from '@/lib/supabase';
+
+export default function Login() {
+
+
+
+}
